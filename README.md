9483843ef3ffe96d8aca125c567a4a4aa753898c
Sirjamo1 <jam_how@hotmail.com>
2022-11-29 10:51:19 +1000

    *added to readme   *found error where '===' was used instead of '=' (caused pause button to not be set)    *minor css changes

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # Image-Slider
+
 Image Slider made for The Odin Project
+<a href="https://github.com/sirjamo1.io/To-Do-List-Odin">Link to live preview</a>
 
+<a href="https://github.com/sirjamo1.io/To-Do-List-Odin"><img src="src/screenshots/screenshot.png" alt="Screenshot" height="400" ></a>
 
+Image Slider was built for The Odin Project.
 
+-   Image changes every 5 seconds.
+-   Photographers name changes with image.
+-   Image alt changes with image.
+-   Dots change with image.
+-   Click image to pause/resume 5 second changes.
+-   Click left/right/dot buttons to change image (also resumes 5 second changes).
+-   Animation pause/play pops up on click.
+-
 
 Images from www.pexels.com
 
 Photographers include :
-                       * Aleksey Kuprikov.
-                       * Amit Kumar
-                       * Chris Janda
-                       * Martin Alargent
-                       * Matthis Volquardsen
-                       * Samet Kasik
+
+-   Aleksey Kuprikov
+-   Amit Kumar
+-   Chris Janda
+-   Martin Alargent
+-   Matthis Volquardsen
+-   Samet Kasik
--- src/main.js
+++ src/main.js
@@ -1,148 +1,162 @@
-
-const images =  ['Samet-Kasik', 'Aleksey-Kuprikov', 'Amit-Kumar', 'Chris-Janda', 'Martin-Alargent', 'Matthis-Volquardsen',
+const images = [
+    "Samet-Kasik",
+    "Aleksey-Kuprikov",
+    "Amit-Kumar",
+    "Chris-Janda",
+    "Martin-Alargent",
+    "Matthis-Volquardsen",
 ];
 let imageIndex = 0;
 let pause = false;
 
 const renderFrame = () => {
-const pictureFrame = document.createElement('div');
-pictureFrame.id = 'picture-frame';
-const image = document.createElement('img');
-image.id = 'current-image'
-image.src = `src/images/${images[imageIndex]}.jpg`;
-image.style.width = "500px";
-const pausePopup = document.createElement('img');
-pausePopup.src = "src/icons/pause-icon.png";
-pausePopup.id = 'pause-popup'
-pictureFrame.appendChild(pausePopup)
-const playPopup = document.createElement("img");
-playPopup.src = "src/icons/play-icon.png";
-playPopup.id = "play-popup";
-pictureFrame.appendChild(playPopup);
-image.onclick = () => {
- pause = !pause;
- pause === true
-     ? ((pausePopup.style.animationName = "play-pause"),
-       (playPopup.style.animationName = ""),
-       clearInterval(pictureAutoChange))
-     : ((pausePopup.style.animationName = ""),
-       (playPopup.style.animationName = "play-pause"), pictureAutoChange = setInterval(() => {
-     pictureAutoChangeInterval();
- }, 5000));
-}
-const leftButton = document.createElement('div');
-leftButton. innerHTML = '<';
-leftButton.id = 'left-btn';
-leftButton.classList.add('arrow-btns');
-leftButton.onclick = () => {
-removeCircleStyles();
-changeImageIndex('left');
- changePicture(image);
- addCircleStyles();
- changePhotographersName()
- continuePlay(pausePopup, playPopup)
-}
-pictureFrame.appendChild(leftButton);
-const rightButton = document.createElement('div');
-rightButton.innerHTML = '>';
-rightButton.id = "right-btn";
-rightButton.classList.add("arrow-btns");
-rightButton.onclick = () => {
-removeCircleStyles();
-changeImageIndex('right');
-changePicture(image);
-addCircleStyles();
-changePhotographersName();
-continuePlay(pausePopup, playPopup)
-}
-pictureFrame.appendChild(createImageCircles(image))
-pictureFrame.appendChild(rightButton);
-pictureFrame.appendChild(image);
-pictureFrame.appendChild(addPhotographersName())
-return pictureFrame
-}
+    const pictureFrame = document.createElement("div");
+    pictureFrame.id = "picture-frame";
+    const image = document.createElement("img");
+    image.id = "current-image";
+    image.src = `src/images/${images[imageIndex]}.jpg`;
+    image.style.width = "500px";
+    image.alt = `Photograph by ${images[imageIndex].replace("-", " ")}`;
+    const pausePopup = document.createElement("img");
+    pausePopup.src = "src/icons/pause-icon.png";
+    pausePopup.id = "pause-popup";
+    pictureFrame.appendChild(pausePopup);
+    const playPopup = document.createElement("img");
+    playPopup.src = "src/icons/play-icon.png";
+    playPopup.id = "play-popup";
+    pictureFrame.appendChild(playPopup);
+    image.onclick = () => {
+        pause = !pause;
+        if (pause === true) {
+            pausePopup.style.animationName = "play-pause";
+            playPopup.style.animationName = "";
+            clearInterval(pictureAutoChange);
+        } else if (pause === false) {
+            pausePopup.style.animationName = "";
+            playPopup.style.animationName = "play-pause";
+            pictureAutoChange = setInterval(() => {
+                pictureAutoChangeInterval();
+            }, 5000);
+        }
+    };
+    const leftButton = document.createElement("div");
+    leftButton.innerHTML = "<";
+    leftButton.id = "left-btn";
+    leftButton.classList.add("arrow-btns");
+    leftButton.onclick = () => {
+        removeCircleStyles();
+        changeImageIndex("left");
+        changePicture(image);
+        addCircleStyles();
+        changePhotographersName();
+        continuePlay(pausePopup, playPopup);
+    };
+    pictureFrame.appendChild(leftButton);
+    const rightButton = document.createElement("div");
+    rightButton.innerHTML = ">";
+    rightButton.id = "right-btn";
+    rightButton.classList.add("arrow-btns");
+    rightButton.onclick = () => {
+        removeCircleStyles();
+        changeImageIndex("right");
+        changePicture(image);
+        addCircleStyles();
+        changePhotographersName();
+        continuePlay(pausePopup, playPopup);
+    };
+    pictureFrame.appendChild(createImageCircles(image));
+    pictureFrame.appendChild(rightButton);
+    pictureFrame.appendChild(image);
+    const photographersNameText = document.createElement("h3");
+    photographersNameText.id = "photographer-name";
+    photographersNameText.innerHTML = `Photographer: ${images[
+        imageIndex
+    ].replace("-", " ")}`;
+    pictureFrame.appendChild(photographersNameText);
+    return pictureFrame;
+};
 const createImageCircles = (image) => {
- const circleContainer = document.createElement('div');
- circleContainer.id = 'circle-container'
- for (let i = 0; i < images.length; i += 1){
-  const indexCircle = document.createElement("div"); 
-  indexCircle.id = `index-circle-${i}`;
-  indexCircle.classList.add('index-circles')
-  if(imageIndex === i) {
-   indexCircle.style.cssText = `
- background-color: blue;
- width: 30px;
- height: 30px;`;
-  }
-  indexCircle.onclick = () => {
-   removeCircleStyles()
-   imageIndex = i
-changePicture(image)
-   addCircleStyles()
-   changePhotographersName()
-   continuePlay(pausePopup, playPopup)
-  }
-  circleContainer.appendChild(indexCircle);
- }
- return circleContainer
-}
+    const circleContainer = document.createElement("div");
+    circleContainer.id = "circle-container";
+    for (let i = 0; i < images.length; i += 1) {
+        const indexCircle = document.createElement("div");
+        indexCircle.id = `index-circle-${i}`;
+        indexCircle.classList.add("index-circles");
+        if (imageIndex === i) {
+            indexCircle.style.cssText = `
+ background-color: rgb(173,216,230);
+ width: 40px;
+ height: 40px;`;
+        }
+        indexCircle.onclick = () => {
+            removeCircleStyles();
+            imageIndex = i;
+            changePicture(image);
+            addCircleStyles();
+            changePhotographersName();
+
+            continuePlay(
+                document.getElementById("pause-popup"),
+                document.getElementById("play-popup")
+            );
+        };
+        circleContainer.appendChild(indexCircle);
+    }
+    return circleContainer;
+};
 const changeImageIndex = (direction) => {
- if (direction === 'right') {
-  imageIndex === 5 ? (imageIndex = 0) : (imageIndex += 1);
- } else if (direction === 'left') {
-  imageIndex === 0
-      ? (imageIndex = images.length - 1)
-      : (imageIndex = imageIndex -= 1);
- }
-}
+    if (direction === "right") {
+        imageIndex === 5 ? (imageIndex = 0) : (imageIndex += 1);
+    } else if (direction === "left") {
+        imageIndex === 0
+            ? (imageIndex = images.length - 1)
+            : (imageIndex = imageIndex -= 1);
+    }
+};
 const removeCircleStyles = () => {
- console.log(imageIndex)
- document.getElementById(`index-circle-${imageIndex}`).style.cssText = `
+    document.getElementById(`index-circle-${imageIndex}`).style.cssText = `
  background-color: white;
- width: 20px;
- height: 20px;`;
- 
-}
+ width: 25px;
+ height: 25px;`;
+};
 const addCircleStyles = () => {
     document.getElementById(`index-circle-${imageIndex}`).style.cssText = `
- background-color: blue;
- width: 30px;
- height: 30px;`;
+ background-color: rgb(173,216,230);
+ width: 40px;
+ height: 40px;`;
 };
 const changePicture = (image) => {
- image.src = `src/images/${images[imageIndex]}.jpg`;
-}
-const addPhotographersName = () => {
-const text = document.createElement('h3');
-text.id = 'photographer-name'
-text.innerHTML = `Photographer: ${images[imageIndex].replace("-", " ")}`;
-return text
-}
+    image.src = `src/images/${images[imageIndex]}.jpg`;
+};
 const changePhotographersName = () => {
-  document.getElementById(
-      "photographer-name"
-  ).innerHTML = `Photographer: ${images[imageIndex].replace("-", " ")}`;
-}
+    document.getElementById(
+        "photographer-name"
+    ).innerHTML = `Photographer: ${images[imageIndex].replace("-", " ")}`;
+    document.getElementById("current-image").alt = `Photograph by ${images[
+        imageIndex
+    ].replace("-", " ")}`;
+};
 let pictureAutoChange = setInterval(() => {
- pictureAutoChangeInterval()
-},5000)
+    pictureAutoChangeInterval();
+}, 5000);
 
 const pictureAutoChangeInterval = () => {
- removeCircleStyles();
- changeImageIndex("right");
- changePicture(document.getElementById("current-image"));
- addCircleStyles();
- changePhotographersName();
-}
+    removeCircleStyles();
+    changeImageIndex("right");
+    changePicture(document.getElementById("current-image"));
+    addCircleStyles();
+    changePhotographersName();
+};
 
 const continuePlay = (pausePopup, playPopup) => {
- if (pause === true) {
-  clearInterval(pictureAutoChange);
-     (pausePopup.style.animationName = "");
-         (playPopup.style.animationName = "play-pause");
-         (pictureAutoChange = setInterval(() => {
-             pictureAutoChangeInterval();
-         }, 5000));
- }
-}
-document.body.appendChild(renderFrame())
+    if (pause === true) {
+        clearInterval(pictureAutoChange);
+        pausePopup.style.animationName = "";
+        playPopup.style.animationName = "play-pause";
+        pictureAutoChange = setInterval(() => {
+            pictureAutoChangeInterval();
+        }, 5000);
+        pause = false;
+    }
+};
+document.body.appendChild(renderFrame());
